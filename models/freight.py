5a9c326a5a36009af6431398e2f94b0a202415fb
yoshi <yoshinori_okubo@outlook.com>
2019-11-13 14:06:24 +0900

    Modify calc_freight func

--- models/freight.py
+++ models/freight.py
@@ -18,12 +18,22 @@ class Freight:
         if history_data is None:
             self.history_data = load_monthly_history_data(FREIGHT_TYPE,CCFI)
 
-    def calc_freight(self,pattern,time):
-        return max(0,self.ship_demand_data[pattern][time]['price']/self.ship_supply_data[pattern][time]['price']*(-1809432.357) + 4770.569817)
+    def calc_freight(self,type,pattern,time):
+        if type == OUTWARD:
+            inclination = -2661522.996
+            intercept = 6834.623141
+            delay = -9
+        elif type == RETURN:
+            inclination = -869948.0385
+            intercept = 2578.502045
+            delay = -3
+        if time - delay < 0 or time - delay > VESSEL_LIFE_TIME*12-1:
+            time = time + delay
+        return max(0,self.ship_demand_data[pattern][time-delay]['price']/self.ship_supply_data[pattern][time-delay]['price']*(inclination) + intercept)
 
 
     # generate predicted sinario
-    def generate_sinario(self,predict_years=DEFAULT_PREDICT_YEARS,predict_pattern_number=DEFAULT_PREDICT_PATTERN_NUMBER):
+    def generate_sinario(self,type,predict_years=DEFAULT_PREDICT_YEARS,predict_pattern_number=DEFAULT_PREDICT_PATTERN_NUMBER):
         # default predict_years is 15 years [180 months]
         self.predict_years  = predict_years
         dt   = np.dtype({'names': ('date', 'price'),
@@ -33,6 +43,6 @@ class Freight:
         for pattern in range(predict_pattern_number):
             for year in range(self.predict_years):
                 for month in range(12):
-                    self.predicted_data = np.append(self.predicted_data, np.array([(year*12+month, self.calc_freight(pattern,year*12+month))], dtype=dt))
+                    self.predicted_data = np.append(self.predicted_data, np.array([(year*12+month, self.calc_freight(type,pattern,year*12+month))], dtype=dt))
         self.predicted_data = self.predicted_data.reshape(DEFAULT_PREDICT_PATTERN_NUMBER,VESSEL_LIFE_TIME*12)
         return
