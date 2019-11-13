f89370439a684e6433366a09428800539b81c773
yoshi <yoshinori_okubo@outlook.com>
2019-11-13 17:10:43 +0900

    Add mimimum_freight

--- models/freight.py
+++ models/freight.py
@@ -6,17 +6,25 @@ import os
 import random
 from ship_demand import ShipDemand
 from ship_supply import ShipSupply
+from ship import Ship
 # import own modules #
 sys.path.append('../public')
 from my_modules import *
 from constants  import *
 
 class Freight:
-    def __init__(self, ship_demand_data,ship_supply_data,history_data=None):
+    def __init__(self, ship_demand_data,ship_supply_data,oil_data,history_data=None):
         self.ship_demand_data = ship_demand_data
         self.ship_supply_data = ship_supply_data
+        self.oil_data = oil_data
         if history_data is None:
             self.history_data = load_monthly_history_data(FREIGHT_TYPE,CCFI)
+
+    def calc_fuel_cost(self,oil):
+        ship = Ship(TEU_SIZE,INITIAL_SPEED,ROUTE_DISTANCE)
+        fuel_cost = ship.calc_fuel_cost(oil)
+        return fuel_cost
+
 
     def calc_freight(self,type,pattern,time):
         if type == OUTWARD:
@@ -29,7 +37,9 @@ class Freight:
             delay = -3
         if time - delay < 0 or time - delay > VESSEL_LIFE_TIME*12-1:
             time = time + delay
-        return max(0,self.ship_demand_data[pattern][time-delay]['price']/self.ship_supply_data[pattern][time-delay]['price']*(inclination) + intercept)
+        fuel_cost = self.calc_fuel_cost(self.oil_data[pattern][time]['price'])
+        minimum_freight = fuel_cost/TEU_SIZE
+        return max(minimum_freight,self.ship_demand_data[pattern][time-delay]['price']/self.ship_supply_data[pattern][time-delay]['price']*(inclination) + intercept)
 
 
     # generate predicted sinario
@@ -44,5 +54,5 @@ class Freight:
             for year in range(self.predict_years):
                 for month in range(12):
                     self.predicted_data = np.append(self.predicted_data, np.array([(year*12+month, self.calc_freight(type,pattern,year*12+month))], dtype=dt))
-        self.predicted_data = self.predicted_data.reshape(DEFAULT_PREDICT_PATTERN_NUMBER,VESSEL_LIFE_TIME*12)
+        self.predicted_data = self.predicted_data.reshape(DEFAULT_PREDICT_PATTERN_NUMBER,predict_years*12)
         return
