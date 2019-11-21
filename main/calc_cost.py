d568472cb6ba2088300b38175aeae54e23433f57
yoshi <yoshinori_okubo@outlook.com>
2019-11-21 21:42:22 +0900

    change the target of comparison

--- main/calc_cost.py
+++ main/calc_cost.py
@@ -9,19 +9,19 @@ from constants  import *
 
 def fitness_function_ave():
     Record = []
-    for pattern in range(DEFAULT_PREDICT_PATTERN_NUMBER):
-        fitness = 0
+    for pattern in range(1):
+        fitness = -INITIAL_COST_OF_SHIPBUIDING
         ship = Ship(TEU_SIZE,INITIAL_SPEED,ROUTE_DISTANCE,1)
         ship.agelist[0] = 0
         for year in range(VESSEL_LIFE_TIME):
             cash_flow = 0
             for month in range(12):
-                current_oil_price = 41.269672901071544
-                current_demand = 10.773913037102997
-                current_freight_rate_outward = 1468.1572093902791
-                current_freight_rate_return = 909.3065453615974
+                current_oil_price = 34
+                current_demand = 10.8
+                current_freight_rate_outward = 1148
+                current_freight_rate_return = 768
                 total_freight =( current_freight_rate_outward * LOAD_FACTOR_ASIA_TO_EUROPE + current_freight_rate_return * LOAD_FACTOR_EUROPE_TO_ASIA)
-                current_supply = 5292.034884937528
+                current_supply = 4734
                 cash_flow += ship.calculate_income_per_month(current_oil_price,total_freight,current_demand,current_supply)
                 cash_flow += ship.add_age()
             DISCOUNT = (1 + DISCOUNT_RATE) ** (year + 1)
@@ -34,7 +34,7 @@ def fitness_function():
     Record = []
     oil_price_data,freight_rate_outward_data,freight_rate_return_data,exchange_rate_data,demand_data,supply_data = load_generated_sinario()
     for pattern in range(DEFAULT_PREDICT_PATTERN_NUMBER):
-        fitness = 0
+        fitness = -INITIAL_COST_OF_SHIPBUIDING
         ship = Ship(TEU_SIZE,INITIAL_SPEED,ROUTE_DISTANCE,1)
         ship.agelist[0] = 0
         for year in range(VESSEL_LIFE_TIME):
@@ -55,4 +55,5 @@ def fitness_function():
     e, sigma = calc_statistics(Record)
     return [e,sigma]
 print(fitness_function_ave()[0]/INITIAL_COST_OF_SHIPBUIDING)
+print(fitness_function()[0]/INITIAL_COST_OF_SHIPBUIDING)
 #print(fitness_function())
