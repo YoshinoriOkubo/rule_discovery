5f09281cc2750631a4c186603b644627122a96a3
yoshi <yoshinori_okubo@outlook.com>
2019-11-27 21:42:27 +0900

    consider load factor into fuel consumption

--- models/ship.py
+++ models/ship.py
@@ -234,8 +234,9 @@ class Ship:
         DSP = 1.37 * DWT + 1660
         k_c0 = 6.87 * 10**(-5)
         k_c1 = 0.65
-        L = 0.78
-        FO = speed_km_h*speed_km_h*k_c0*(DSP-DWT+k_c1*L*DWT)*DSP**(-1/3)
+        FO = speed_km_h*speed_km_h*k_c0*(DSP-DWT+k_c1*LOAD_FACTOR_ASIA_TO_EUROPE*DWT)*DSP**(-1/3)
+        FO += speed_km_h*speed_km_h*k_c0*(DSP-DWT+k_c1*LOAD_FACTOR_EUROPE_TO_ASIA*DWT)*DSP**(-1/3)
+        FO /= 2.0
         return FO #kg/km
         #http://www.nilim.go.jp/lab/bcg/siryou/tnn/tnn0494pdf/ks049404.pdf
 
